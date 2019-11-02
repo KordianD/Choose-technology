43aa5c0a06f8343959c240dcb19754e4e77d3984
Adam Latos <latosadam1@gmail.com>
2019-11-02 22:59:10 +0000

    add some techs

---
+++ data.pl
@@ -0,0 +1,65 @@
+% technologies - end results:
+
+tech(react) :-
+        field(web_frontend),
+        lang(javascript),
+        skill(intermediate).
+
+tech(angular) :-
+        field(web_frontend),
+        lang(typescript),
+        skill(intermediate).
+        
+tech(vue) :-
+        field(web_frontend),
+        lang(javascript),
+        skill(intermediate).
+
+tech(css_js) :-
+        field(web_frontend),
+        lang(javascript),
+        skill(basic).
+
+tech(django) :-
+        field(web_backend),
+        lang(python),
+        skill(intermediate).
+
+tech(flask) :-
+        field(web_backend),
+        lang(python),
+        skill(basic).
+
+tech(electron) :-
+        field(desktop),
+        lang(javascript),
+        platform(any).
+
+tech(wpf) :-
+        field(desktop),
+        lang(csharp),
+        platform(windows).
+
+tech(asp) :-
+        field(web_frontend),
+        lang(csharp).
+
+tech(pytorch) :-
+        field(machine_learning),
+        lang(python).
+
+tech(tensorflow) :-
+        field(machine_learning),
+        lang(python).
+
+tech(dl4j) :-
+        field(machine_learning),
+        lang(java).
+
+tech(unity) :-
+        field(gamedev),
+        lang(csharp).
+
+tech(unreal) :-
+        field(gamedev),
+        lang(cpp).
